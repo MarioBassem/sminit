032b46eb82a3ebdd212dab29180daf88bded020b
mariobassem <mariobassem12@gmail.com>
2022-12-28 17:54:44 +0200

    implements loader and loader tests
    Signed-off-by: mariobassem <mariobassem12@gmail.com>

--- swatcher/loader.go
+++ swatcher/loader.go
@@ -1,4 +1,58 @@
 package swatcher
 
-// load is responsible for loading all services from /etc/sminit into multiple `Service` structs
-func (m Swatcher) Load() error
+import (
+	"os"
+	"path"
+
+	"github.com/pkg/errors"
+	"gopkg.in/yaml.v3"
+)
+
+// LoadAll is responsible for loading all services from /etc/sminit into multiple UserDefinedService structs
+// Users should provide their service definition yaml files in /etc/sminit
+// Load ignores any subdirectories in /etc/sminit, or any non-regular files.
+func (s Swatcher) LoadAll(dirPath string) ([]UserDefinedService, error) {
+	entries, err := os.ReadDir(dirPath)
+	if err != nil {
+		return nil, errors.Wrapf(err, "couldn't read entries of %s.", dirPath)
+	}
+
+	services := []UserDefinedService{}
+
+	for _, entry := range entries {
+		if !entry.Type().IsRegular() {
+			continue
+		}
+
+		name := entry.Name()
+		userDefinedService, err := s.Load(dirPath, name)
+		if err != nil {
+			return nil, errors.Wrapf(err, "couldn't load service %s.", name)
+		}
+
+		services = append(services, userDefinedService)
+	}
+
+	return services, nil
+}
+
+// Load is responsible for loading a service from /etc/sminit with a provided serviceName into a UserDefinedService struct.
+func (s Swatcher) Load(dirPath, serviceName string) (UserDefinedService, error) {
+	path := path.Join(dirPath, serviceName)
+
+	bytes, err := os.ReadFile(path)
+	if err != nil {
+		return UserDefinedService{}, errors.Wrapf(err, "couldn't read contents of file %s.", path)
+	}
+
+	userDefinedServide := UserDefinedService{
+		Name: serviceName,
+	}
+
+	err = yaml.Unmarshal(bytes, &userDefinedServide)
+	if err != nil {
+		return UserDefinedService{}, errors.Wrapf(err, "couldn't unmarshal contents of file %s.", path)
+	}
+
+	return userDefinedServide, nil
+}
---
+++ swatcher/loader_test.go
@@ -0,0 +1,64 @@
+package swatcher
+
+import (
+	"os"
+	"path"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/yaml.v3"
+)
+
+func TestLoader(t *testing.T) {
+	tmpDir := t.TempDir()
+	want := []UserDefinedService{
+		{
+			Name:   "s1",
+			Cmd:    "echo hi",
+			Before: []string{"s2", "s3"},
+			After:  []string{"s4", "s5"},
+			Log:    "log1",
+		},
+		{
+			Name:   "s2",
+			Cmd:    "echo \"hello world\"",
+			Before: []string{"s1", "s3"},
+			After:  []string{"s2", "s4"},
+		},
+	}
+
+	for _, service := range want {
+		path := path.Join(tmpDir, service.Name)
+		file, err := os.Create(path)
+		if err != nil {
+			t.Errorf("couldn't create definition file for service %s. %s", service.Name, err.Error())
+		}
+
+		bytes, err := yaml.Marshal(service)
+		if err != nil {
+			t.Errorf("couldn't marshal service %s to bytes. %s", service.Name, err.Error())
+		}
+
+		_, err = file.Write(bytes)
+		if err != nil {
+			t.Errorf("couldn't write bytes. %s", err.Error())
+		}
+	}
+	t.Run("load all", func(t *testing.T) {
+		swathcer, err := NewSwatcher()
+		assert.NoError(t, err)
+
+		services, err := swathcer.LoadAll(tmpDir)
+		assert.NoError(t, err)
+		assert.Equal(t, want, services)
+	})
+
+	t.Run("load one", func(t *testing.T) {
+		swatcher, err := NewSwatcher()
+		assert.NoError(t, err)
+
+		service, err := swatcher.Load(tmpDir, want[0].Name)
+		assert.NoError(t, err)
+		assert.Equal(t, want[0], service)
+	})
+}
